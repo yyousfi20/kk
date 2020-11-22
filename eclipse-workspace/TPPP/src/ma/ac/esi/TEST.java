709ce8dfc9c4e7d6987f34820daa58bb6e865f65
lamiae <sebbarhlamiae@gmail.com>
2020-11-22 12:28:56 +0100

    First commit

---
+++ eclipse-workspace/TPPP/src/ma/ac/esi/TEST.java
@@ -0,0 +1,15 @@
+package ma.ac.esi;
+
+public class TEST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				System.out.println("Hello World"); 
+			}
+
+		
+
+	}
+
+
